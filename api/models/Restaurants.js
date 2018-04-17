7c11964f83d0d2b85667a56c5cebcbe7b0a273bb
Abdelrahman Awad <logaretm1@gmail.com>
2018-04-17 16:17:06 +0200

    fix: missing comma in json

--- api/models/Restaurants.js
+++ api/models/Restaurants.js
@@ -1,8 +1,7 @@
 /**
  * Restaurants.js
  *
- * @description :: TODO: You might write a short summary of how this model works and what it represents here.
- * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ * @description :: This represents the restaurant entry in the database.
  */
 
 module.exports = {
--- api/models/Reviews.js
+++ api/models/Reviews.js
@@ -1,14 +1,13 @@
 /**
  * Reviews.js
  *
- * @description :: TODO: You might write a short summary of how this model works and what it represents here.
- * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ * @description :: This represents the review entry in the database.
  */
 
 module.exports = {
   attributes: {
     name: { type: 'string' },
-    comments: { type: 'string' }
+    comments: { type: 'string' },
     rating: { type: 'number', min: 0, max: 5 },
     restaurant_id: { type: 'number', required: true },
     restaurant: { model: 'restaurants', via: 'restaurant_id' },
