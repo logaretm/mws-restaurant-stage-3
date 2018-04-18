db2019dc80ebb39f4b5001087443ae52b6799594
Abdelrahman Awad <logaretm1@gmail.com>
2018-04-18 15:01:53 +0200

    feat: simple data source store for offline population

---
+++ assets/js/store.js
@@ -0,0 +1,44 @@
+import idb from './idb';
+
+const state = {
+  restaurants: [],
+  reviews: [],
+  neighborhoods: [],
+  cuisines: []
+}
+
+let populated = false;
+
+/**
+ * Simple Data Store, I could use Vuex but its not really needed.
+ */
+export default class Store {
+  static populate () {
+    if (populated) {
+      return Promise.resolve(state);
+    }
+
+    return Promise.all([
+      idb.getCachedCollection('restaurants'),
+      idb.getCachedCollection('reviews'),
+      idb.getCachedCollection('neighborhoods'),
+      idb.getCachedCollection('cuisines'),
+    ]).then([restaurants, reviews, neighborhoods, cuisines] => {
+      state.restaurants = restaurants;
+      state.reviews = reviews;
+      state.neighborhoods = neighborhoods;
+      state.cuisines = cuisines;
+      populated = true;
+    }).then(() => {
+      return state;
+    });
+  }
+
+  static get state () {
+    return state;
+  }
+
+  static find (storeName, id) {
+    return idb.getCachedItem(storeName, id);
+  }
+}
