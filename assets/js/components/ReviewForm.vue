8bbac739ed7ed0086d7ec200bd51f94857898f6b
Abdelrahman Awad <logaretm1@gmail.com>
2018-05-06 19:41:40 +0200

    feat: style review form

--- assets/js/components/ReviewForm.vue
+++ assets/js/components/ReviewForm.vue
@@ -1,17 +1,19 @@
 <template>
   <form action="" @submit.prevent="onSubmit">
-    <label>
-      Name
-      <input type="text" name="name" placeholder="Your Name" required v-model="name">
-    </label>
-    <label>
-      Comments
-      <textarea name="comments" rows="5" :placeholder="`Describe your experience at ${restaurantName}`" required v-model="comments"></textarea>
-    </label>
-    <label>
-      Rating
-      <input type="numeric" max="5" min="1" step="1" v-model.number="rating">
-    </label>
+      <div class="field">
+        <label class="form__label">Name</label>
+        <input class="input" type="text" name="name" placeholder="Your Name" required v-model="name">
+      </div>
+      <div class="field">
+        <label class="form__label">Comments</label>
+        <textarea class="input" name="comments" rows="5" :placeholder="`Describe your experience at ${restaurantName}`" required v-model="comments"></textarea>
+      </div>
+      <div class="field">
+        <label class="form__label">Rating</label>
+        <input class="input" type="numeric" max="5" min="1" step="1" v-model.number="rating">
+      </div>
+
+      <button type="submit" class="form__submit">Submit</button>
   </form>
 </template>
 
@@ -64,3 +66,47 @@ export default {
   }
 };
 </script>
+
+<style lang="stylus">
+.field
+  display block
+  &:not(:last-child)
+    margin-bottom .75rem
+
+.form__label
+  font-size 1rem
+  display block
+  &:not(:last-child)
+    margin-bottom .5em
+
+.form__submit
+  background-color #2366d1
+  border-color transparent
+  color #fff
+  cursor pointer
+  justify-content center
+  padding-bottom calc(.375em - 1px)
+  padding-left .75em
+  padding-right .75em
+  padding-top calc(.375em - 1px)
+  text-align center
+  white-space nowrap
+  font-size 1rem
+  border-radius 2px
+
+.input
+  border 1px solid transparent
+  font-size 1rem
+  position relative
+  max-width 100%
+  width 100%
+  padding-bottom calc(.375em - 1px)
+  padding-left calc(.625em - 1px)
+  padding-right calc(.625em - 1px)
+  padding-top calc(.375em - 1px)
+  border-radius 2px
+  &:invalid
+    border-color #ff3860
+  &:valid
+    border-color #23d160
+</style>
