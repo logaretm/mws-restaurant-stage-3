5cd0c4a37eceef078ed00710bbaaa59677dcd463
Abdelrahman Awad <logaretm1@gmail.com>
2018-06-04 23:24:40 +0200

    linting

--- scripts/images.js
+++ scripts/images.js
@@ -5,13 +5,11 @@ const webp = require('imagemin-webp');
 const output = path.resolve(__dirname, '../public/img');
 const jpgs = path.resolve(__dirname, '../assets/img/*.jpg');
 
-
 imagemin([jpgs], output, {
   plugins: [webp({
     quality: 70 // Quality setting from 0 to 100
   })]
 });
-
 
 imagemin([jpgs], output, {
   plugins: [webp({
